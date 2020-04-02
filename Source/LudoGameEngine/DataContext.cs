e76950141c6e2a825be137108f62e01a446089a5
samir19ehsani <samirehsani19@gmail.com>
2020-04-02 18:15:25 +0200

    Driver @samirehsani19

--- Source/LudoGameEngine/DataContext.cs
+++ Source/LudoGameEngine/DataContext.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LudoGameEngine
 {
-    class DataContext : DbContext
+    public class DataContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("AppSettings.Json")
+                .Build();
+            var defaultConnection = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(defaultConnection);
+        }
     }
 }
